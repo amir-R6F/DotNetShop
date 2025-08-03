d717d6dfdb35902e537fca6849294a7e5a021fee
melika <md.r6f.mrg@gmail.com>
2025-08-03 17:18:37 +0430

    add ColleagueDiscountApplication.cs

--- Shop/Dm.Application/ColleagueDiscountApplication.cs
+++ Shop/Dm.Application/ColleagueDiscountApplication.cs
@@ -17,32 +17,74 @@ namespace Dm.Application
 
         public OperationResult Define(DefineColleagueDiscount command)
         {
-            throw new System.NotImplementedException();
+            var operation = new OperationResult();
+            if (_colleagueDiscountRepository.Exists(x =>
+                x.ProductId == command.ProductId &&
+                x.DiscountRate == command.DiscountRate))
+                return operation.Failed(ApplicationMessages.Duplicate);
+
+            var colleaguediscount = new ColleagueDiscount(command.ProductId, command.DiscountRate);
+            _colleagueDiscountRepository.Create(colleaguediscount);
+            _colleagueDiscountRepository.SaveChanges();
+            return operation.Succedded();
         }
 
         public OperationResult Edit(EditColleagueDiscount command)
         {
-            throw new System.NotImplementedException();
+            var operation = new OperationResult();
+
+            var colleaguediscount = _colleagueDiscountRepository.Get(command.Id);
+
+            if (colleaguediscount == null)
+                return operation.Failed(ApplicationMessages.NotFound);
+            
+            if (_colleagueDiscountRepository.Exists(x =>
+                x.ProductId == command.ProductId &&
+                x.DiscountRate == command.DiscountRate &&
+                x.Id != command.Id))
+                return operation.Failed(ApplicationMessages.Duplicate);
+
+            colleaguediscount.Edit(command.ProductId, command.DiscountRate);
+            _colleagueDiscountRepository.SaveChanges();
+            return operation.Succedded();
         }
 
         public EditColleagueDiscount GetDetails(long id)
         {
-            throw new System.NotImplementedException();
+            return _colleagueDiscountRepository.GetDetails(id);
         }
 
         public List<ColleagueDiscountViewModel> Search(ColleagueDiscountSearchModel searchModel)
         {
-            throw new System.NotImplementedException();
+            return _colleagueDiscountRepository.Search(searchModel);
         }
 
         public OperationResult Remove(long id)
         {
-            throw new System.NotImplementedException();
+            var operation = new OperationResult();
+
+            var colleaguediscount = _colleagueDiscountRepository.Get(id);
+
+            if (colleaguediscount == null)
+                return operation.Failed(ApplicationMessages.NotFound);
+
+            colleaguediscount.Remove();
+            _colleagueDiscountRepository.SaveChanges();
+            return operation.Succedded();
         }
 
         public OperationResult Restore(long id)
         {
-            throw new System.NotImplementedException();
+            var operation = new OperationResult();
+
+            var colleaguediscount = _colleagueDiscountRepository.Get(id);
+
+            if (colleaguediscount == null)
+                return operation.Failed(ApplicationMessages.NotFound);
+
+            colleaguediscount.Restore();
+            _colleagueDiscountRepository.SaveChanges();
+            return operation.Succedded();
         }
     }
 }
